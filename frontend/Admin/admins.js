9941b897eef4ade2017c8cb4ba3a338b737504ec
RedHairedShanks07 <vbhat123321@gmail.com>
2025-04-30 14:43:23 +0530

    Update admins.js

--- frontend/Admin/admins.js
+++ frontend/Admin/admins.js
@@ -1,7 +1,10 @@
 const admins = [
-    "gangadharangadi@rvce.edu.in",
-    "roopats@rvce.edu.in",
-    "natarajjr@rvce.edu.in"
-]
+  "gangadharangadi@rvce.edu.in",
+  "roopats@rvce.edu.in",
+  "natarajjr@rvce.edu.in",
+  "dollyjagadish263@gmail.com",
+  // "vishalkbhat.cs23@rvce.edu.in",
+  // "vssreenivaas.cs23@rvce.edu.in",
+];
 
 export default admins;
